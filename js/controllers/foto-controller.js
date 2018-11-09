cf9242600602b6de0c54dec4101820d1aa162ddd
Vainer <vainer_fabri@hotmail.com>
2018-11-09 14:02:28 -0200

    Facilitando backend

--- js/controllers/foto-controller.js
+++ js/controllers/foto-controller.js
@@ -1,34 +1,36 @@
 angular
   .module("alurapic")
-  .controller("FotoController", function($scope, $http, $routeParams) {
+  .controller("FotoController", function($scope, recursoFoto, $routeParams) {
     $scope.foto = {};
     $scope.mensagem = "";
 
     if ($routeParams.fotoId) {
-      $http
-        .get("/v1/fotos/" + $routeParams.fotoId)
-        .success(function(foto) {
+      recursoFoto.get(
+        { fotoId: $routeParams.fotoId },
+        function(foto) {
           $scope.foto = foto;
-        })
-        .error(function(erro) {
+        },
+        function(erro) {
           console.log(erro);
           $scope.mensagem = "Não foi possível obter a foto";
-        });
+        }
+      );
     }
 
     $scope.submeter = function() {
       if ($scope.formulario.$valid) {
         if ($routeParams.fotoId) {
-          $http
-            .put("/v1/fotos/" + $scope.foto._id, $scope.foto)
-            .success(function() {
+          recursoFoto.update(
+            { fotoId: $scope.foto._id },
+            $scope.foto,
+            function() {
               $scope.mensagem = "Foto " + $scope.foto.titulo + " foi alterada";
-            })
-            .error(function(erro) {
-              console.log(erro);
+            },
+            function(erro) {
               $scope.mensagem =
                 "Não foi possível alterar a foto " + $scope.foto.titulo;
-            });
+            }
+          );
         } else {
           $http
             .post("/v1/fotos", $scope.foto)
